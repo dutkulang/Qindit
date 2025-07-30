65b7c4b47c1ac5dc764a15b65d266762fdf91ad2
Dut Kulang <dutandrew78@gmail.com>
2025-07-30 17:30:23 +0200

    register food models to admin

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from food.models import Restaurant, MenuItem, Order, OrderItem
 
-# Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(MenuItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
