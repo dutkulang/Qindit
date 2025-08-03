ddd26e0db58f4b67b1cfadf0ec8e28252e1fcc03
Dut Kulang <dutandrew78@gmail.com>
2025-08-03 15:36:50 +0200

    created views to handle business logic
    static houses the project css file
    mapped app urls to that of the project level

--- food/views.py
+++ food/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'restaurants': models.Restaurant.objects.all()
+    }
+    
+    return render(request, 'main.html', context)
+
+
+def foodItems(request):
+    context = {
+        "foodItems": models.MenuItem.objects.all()
+    }
+    return render(request, 'foods.html', context )
